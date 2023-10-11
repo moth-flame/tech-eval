7bfefe4988806658684bc7e633f5a4bafeda9c9b
Cody Coker <cody@mothandflamevr.com>
2023-10-11 13:55:48 -0400

    add palindrome script

---
+++ Assets/Scripts/Palindrome.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Palindrome : MonoBehaviour
+{
+    private void Start()
+    {
+        // Take an array of the following words and log out whether each one is a palindrome or not.
+        
+        // Palindrome words
+        var string1 = "racecar";
+        var string2 = "level";
+        var string3 = "kayak";
+        var string4 = "reviver";
+        var string5 = "madam";
+
+        // Non-palindrome words
+        var string6 = "hello";
+        var string7 = "world";
+        var string8 = "goodbye";
+        var string9 = "cruel";
+        var string10 = "earth";
+        
+        var words = new string[] { string1, string2, string3, string4, string5, string6, string7, string8, string9, string10 };
+        
+        // Your code here!
+        
+        
+    }
+}
